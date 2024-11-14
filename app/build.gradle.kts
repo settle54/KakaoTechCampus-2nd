b11e5b2f72e1f4d432ac4c6994893654ac772a7a
todlf <todlfdl@naver.com>
2024-11-14 11:40:28 +0900

    refactor: release 버전 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "potatocake.katecam.everymoment"
         minSdk = 28
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
